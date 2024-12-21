d4a5577dd0ab3377df849b0f7256385f6981cd25
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-12-21 11:26:57 +0100

    Update

---
+++ functions.sh
@@ -0,0 +1,31 @@
+# Functions for Fedora
+
+# Terra repos
+function f_terra(){
+  echo "function"
+}
+
+# RPM-fusion
+function f_rpmfusion(){
+  echo "function"
+}
+
+# Fedora auto updates
+function f_updates(){
+  echo "function"
+}
+
+# Flatpak auto updates
+function f_flatpak(){
+  echo "function"
+}
+
+# Multimedia
+function f_multimedia(){
+  echo "function"
+}
+
+# Gaming
+function f_gaming(){
+  echo "function"
+}
