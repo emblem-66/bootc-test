0725115371a29727638bcd83f7972e2c1770aea4
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-12-23 12:11:43 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,10 @@ set -ouex pipefail
 
 source <(curl -s "https://raw.githubusercontent.com/Emblem-66/bootc-test/refs/heads/main/functions.sh")
 
+rpm -qa | grep mesa
 
 f_terra
+
+rpm -qa | grep kernel
 
 echo "done"
