aaf834ba5e01a590f3d974c6c61873a3a28aa4b0
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-12-23 12:23:46 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -17,25 +17,25 @@ function f_updates(){
   sudo sed -i 's/#LockLayering=false/LockLayering=true/' /etc/rpm-ostreed.conf
   systemctl enable rpm-ostreed-automatic.timer
   cat <<EOF | sudo tee /etc/systemd/system/flatpak-update.service > /dev/null
-  [Unit]
-  Description=Update Flatpaks
-  [Service]
-  Type=oneshot
-  ExecStart=/usr/bin/flatpak update -y
-  [Install]
-  WantedBy=default.target
-  EOF
+[Unit]
+Description=Update Flatpaks
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/flatpak update -y
+[Install]
+WantedBy=default.target
+EOF
   systemctl enable flatpak-update.service
   
   cat <<EOF | sudo tee /etc/systemd/system/flatpak-update.timer > /dev/null
-  [Unit]
-  Description=Update Flatpaks
-  [Timer]
-  OnCalendar=*:0/4
-  Persistent=true
-  [Install]
-  WantedBy=timers.target
-  EOF
+[Unit]
+Description=Update Flatpaks
+[Timer]
+OnCalendar=*:0/4
+Persistent=true
+[Install]
+WantedBy=timers.target
+EOF
   systemctl enable flatpak-update.timer
 }
 
