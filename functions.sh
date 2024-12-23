ded1e9ab93739f7e0a6996119b30afcfc48f0228
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-12-23 12:19:25 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -2,17 +2,41 @@
 
 # Terra repos
 function f_terra(){
-  echo "function"
+  curl -o /etc/yum.repos.d/terra.repo "https://raw.githubusercontent.com/terrapkg/subatomic-repos/main/terra.repo"
+  dnf install -y terra-release
 }
 
 # RPM-fusion
 function f_rpmfusion(){
-  echo "function"
+  dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 }
 
 # Fedora auto updates
 function f_updates(){
-  echo "function"
+  sudo sed -i 's/#AutomaticUpdatePolicy=none/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
+  sudo sed -i 's/#LockLayering=false/LockLayering=true/' /etc/rpm-ostreed.conf
+  systemctl enable rpm-ostreed-automatic.timer
+  cat <<EOF | sudo tee /etc/systemd/system/flatpak-update.service > /dev/null
+  [Unit]
+  Description=Update Flatpaks
+  [Service]
+  Type=oneshot
+  ExecStart=/usr/bin/flatpak update -y
+  [Install]
+  WantedBy=default.target
+  EOF
+  systemctl enable flatpak-update.service
+  
+  cat <<EOF | sudo tee /etc/systemd/system/flatpak-update.timer > /dev/null
+  [Unit]
+  Description=Update Flatpaks
+  [Timer]
+  OnCalendar=*:0/4
+  Persistent=true
+  [Install]
+  WantedBy=timers.target
+  EOF
+  systemctl enable flatpak-update.timer
 }
 
 # Flatpak auto updates
@@ -27,5 +51,10 @@ function f_multimedia(){
 
 # Gaming
 function f_gaming(){
+  echo "function"
+}
+
+# GNOME
+function f_gnome(){
   echo "function"
 }
