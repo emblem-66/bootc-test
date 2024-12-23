37e72108f2eba51906c1a64d9cd11b1a22377f5c
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-12-23 13:56:45 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -8,11 +8,8 @@ function f_terra(){
 
 # RPM-fusion
 function f_rpmfusion(){
-  rpmfusion=(
-    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-  )
-  dnf install -y ${rpmfusion[@]}
+  dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 }
 
 # Fedora auto updates
