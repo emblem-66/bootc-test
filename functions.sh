195ddd8e014748cec4c95f0aae5152385efd0fca
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-12-24 22:01:15 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -92,7 +92,7 @@ function f_gaming(){
 	#dnf copr enable -y faugus/faugus-launcher
 	#dnf install -y faugus-launcher
 	# Local RPM - Heroic
-	wget -O heroic-latest.rpm $(curl -s https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases/latest | jq -r '.assets[] | select(.name | contains ("rpm")) | .browser_download_url')
+	#wget -O heroic-latest.rpm $(curl -s https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases/latest | jq -r '.assets[] | select(.name | contains ("rpm")) | .browser_download_url')
 	#dnf install -y heroic-latest.rpm
 
 	rpm -qa | sort | grep gamescope
